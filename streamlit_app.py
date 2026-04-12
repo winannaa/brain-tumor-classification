97e804cece3da38f99646b053bd2b366bf7f4dfd
Wina <117616393+winannaa@users.noreply.github.com>
2026-04-12 16:24:12 +0700

    Refactor streamlit_app.py by cleaning comments
    Removed unnecessary comments and updated title formatting.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import numpy as np
 from PIL import Image
 import onnxruntime as ort
 
-# =========================
-# LOAD MODEL
-# =========================
 @st.cache_resource
 def load_model():
     session = ort.InferenceSession("model/model.onnx")
@@ -16,14 +13,8 @@ session = load_model()
 input_name = session.get_inputs()[0].name
 output_name = session.get_outputs()[0].name
 
-# =========================
-# LABEL
-# =========================
 class_names = ["Glioma", "Meningioma", "Pituitary", "No Tumor"]
 
-# =========================
-# PREPROCESSING
-# =========================
 def preprocess_image(image):
     img = image.resize((224, 224))
     img = np.array(img).astype(np.float32)
@@ -31,10 +22,7 @@ def preprocess_image(image):
     img = np.expand_dims(img, axis=0)
     return img
 
-# =========================
-# UI
-# =========================
-st.title("🧠 Brain Tumor Classification")
+st.title("Brain Tumor Classification")
 
 uploaded_file = st.file_uploader("Upload MRI Image", type=["jpg", "png", "jpeg"])
 
